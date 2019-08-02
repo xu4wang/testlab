39c53946d5fc4cce5a124608f7a2dee008473c34
Wang Xu <wangxu@mail.dspread.com>
2019-08-03 02:17:12 +0700

    sleekxmpp echo demo

---
+++ xmpp/echo_xmpp.py
@@ -0,0 +1,33 @@
+import logging
+
+from sleekxmpp import ClientXMPP
+from sleekxmpp.exceptions import IqError, IqTimeout
+
+
+class EchoBot(ClientXMPP):
+
+    def __init__(self, jid, password):
+        ClientXMPP.__init__(self, jid, password)
+
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler("message", self.message)
+
+    def session_start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+
+    def message(self, msg):
+        if msg['type'] in ('chat', 'normal'):
+            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)-8s %(message)s')
+
+    xmpp = EchoBot('echo@wx.alixpay.com', 'echo')
+    xmpp.connect()
+    xmpp.process(block=True)
+
